14a26a2983614dc97b9a2674adab6187444fe696
Tim Krones <tim@opencraft.com>
2016-12-01 18:59:16 +0100

    Version bump (v1.2.3).

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def package_data(pkg, roots):
 
 setup(
     name='xblock-poll',
-    version='1.2.2',
+    version='1.2.3',
     description='An XBlock for polling users.',
     packages=[
         'poll',
