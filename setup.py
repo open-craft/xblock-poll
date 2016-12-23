a35015b13291f31519999c860a3873fc3122686a
Zia Fazal <zia.fazal@arbisoft.com>
2016-12-23 17:07:59 +0500

    ziafazal/WL-580: added translation files and transifex config

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def package_data(pkg, roots):
 
 setup(
     name='xblock-poll',
-    version='1.2.3',
+    version='1.2.4',
     description='An XBlock for polling users.',
     packages=[
         'poll',
