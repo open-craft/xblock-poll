a63bdcdec9bcda32c6ac858ce7bf65870f5400b6
murad <murad.habib@arbisoft.com>
2017-09-12 19:44:56 +0500

    Polls UI fixes for mobile

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def package_data(pkg, roots):
 
 setup(
     name='xblock-poll',
-    version='1.3.2',
+    version='1.3.3',
     description='An XBlock for polling users.',
     packages=[
         'poll',
