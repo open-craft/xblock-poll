4e9ebaf77658482ca15d7b62133114e051a6e2b1
Moeez Zahid <moeezzahid1996@gmail.com>
2018-11-28 16:51:40 +0500

    MCKIN-8799 Download button enabled only when final download url is ready (#48)

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def package_data(pkg, roots):
 
 setup(
     name='xblock-poll',
-    version='1.6.1',
+    version='1.6.2',
     description='An XBlock for polling users.',
     packages=[
         'poll',
