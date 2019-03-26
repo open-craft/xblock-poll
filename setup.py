8b06cadb103670162d7dbc3689d0f3084f431efc
Kshitij Sobti <kshitij@sobti.in>
2019-03-27 05:13:17 +0530

    Use completion events instead of progress

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def package_data(pkg, roots):
 
 setup(
     name='xblock-poll',
-    version='1.8.2',
+    version='1.8.3',
     description='An XBlock for polling users.',
     packages=[
         'poll',
