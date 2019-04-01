63992cb3ef9c3581254605882d2cd163ac7b1dfb
Muhammad Usman <43761905+musmanmalik@users.noreply.github.com>
2019-04-02 04:25:09 +0500

    Translation of word "FEEDBACK" updated (#56)

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def package_data(pkg, roots):
 
 setup(
     name='xblock-poll',
-    version='1.8.3',
+    version='1.8.4',
     description='An XBlock for polling users.',
     packages=[
         'poll',
