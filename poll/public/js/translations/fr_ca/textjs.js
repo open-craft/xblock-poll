0d0710adaf61c96b4bf58533fd13381b875d2b81
Omar Al-Ithawi <i@omardo.com>
2019-08-28 18:43:13 +0300

    Update translations (autogenerated message)

--- poll/public/js/translations/fr_ca/textjs.js
+++ poll/public/js/translations/fr_ca/textjs.js
@@ -20,13 +20,62 @@
   
 
   
-  /* gettext identity library */
+  /* gettext library */
 
-  django.gettext = function (msgid) { return msgid; };
-  django.ngettext = function (singular, plural, count) { return (count == 1) ? singular : plural; };
+  django.catalog = {
+    "Answer": "R\u00e9ponse", 
+    "Delete": "Supprimer", 
+    "Feedback": "Commentaire", 
+    "Image URL": "URL de l'image", 
+    "Image alternative text": "Texte alternatif de l'image", 
+    "Question": "Question", 
+    "Results": "R\u00e9sultats", 
+    "Results gathered from {total} respondent.": [
+      "R\u00e9sultats recueillis aupr\u00e8s de {total} r\u00e9pondant.", 
+      "R\u00e9sultats recueillis aupr\u00e8s de {total} r\u00e9pondants."
+    ], 
+    "Submit": "Soumettre", 
+    "This must have an image URL or text, and can have both.  If you add an image, you must also provide an alternative text that describes the image in a way that would allow someone to answer the poll if the image did not load.": "Cela doit avoir une URL d'image ou un texte, et peut avoir les deux. Si vous ajoutez une image, vous devez \u00e9galement fournir un texte de remplacement d\u00e9crivant l'image de mani\u00e8re \u00e0 permettre \u00e0 quelqu'un de r\u00e9pondre au sondage si l'image ne s'est pas charg\u00e9e.", 
+    "You can make limited use of Markdown in answer texts, preferably only bold and italics.": "Vous pouvez utiliser Markdown de mani\u00e8re limit\u00e9e dans les textes de r\u00e9ponse, de pr\u00e9f\u00e9rence uniquement en gras et en italique.", 
+    "move poll down": "d\u00e9placer le sondage vers le bas", 
+    "move poll up": "d\u00e9placer le sondage vers le haut"
+  };
+
+  django.gettext = function (msgid) {
+    var value = django.catalog[msgid];
+    if (typeof(value) == 'undefined') {
+      return msgid;
+    } else {
+      return (typeof(value) == 'string') ? value : value[0];
+    }
+  };
+
+  django.ngettext = function (singular, plural, count) {
+    var value = django.catalog[singular];
+    if (typeof(value) == 'undefined') {
+      return (count == 1) ? singular : plural;
+    } else {
+      return value[django.pluralidx(count)];
+    }
+  };
+
   django.gettext_noop = function (msgid) { return msgid; };
-  django.pgettext = function (context, msgid) { return msgid; };
-  django.npgettext = function (context, singular, plural, count) { return (count == 1) ? singular : plural; };
+
+  django.pgettext = function (context, msgid) {
+    var value = django.gettext(context + '\x04' + msgid);
+    if (value.indexOf('\x04') != -1) {
+      value = msgid;
+    }
+    return value;
+  };
+
+  django.npgettext = function (context, singular, plural, count) {
+    var value = django.ngettext(context + '\x04' + singular, context + '\x04' + plural, count);
+    if (value.indexOf('\x04') != -1) {
+      value = django.ngettext(singular, plural, count);
+    }
+    return value;
+  };
   
 
   django.interpolate = function (fmt, obj, named) {
