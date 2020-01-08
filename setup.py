81344bc6afb739bfeb6dc67a552af46f1e845a19
moeez96 <moeezzahid1996@gmail.com>
2020-01-08 19:48:29 +0500

    Update setup version

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def package_data(pkg, roots):
 
 setup(
     name='xblock-poll',
-    version='1.9.1',
+    version='1.9.2',
     description='An XBlock for polling users.',
     packages=[
         'poll',
