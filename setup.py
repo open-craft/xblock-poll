e66cd1af9e3d807281b834e74276b7a982d59282
Agrendalath <piotr@surowiec.it>
2020-03-23 11:56:38 +0100

    [BB-2174] Bump version

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def package_data(pkg, roots):
 
 setup(
     name='xblock-poll',
-    version='1.9.3',
+    version='1.9.4',
     description='An XBlock for polling users.',
     packages=[
         'poll',
