8997359819ba66abc1f647a364d6248b8aec646b
musmanmalik <m.usman@arbisoft.com>
2020-07-09 19:48:41 +0500

    French Translation correction

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def package_data(pkg, roots):
 
 setup(
     name='xblock-poll',
-    version='1.9.9',
+    version='1.9.10',
     description='An XBlock for polling users.',
     packages=[
         'poll',
